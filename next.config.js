09ac8f19f97243907d51340751823c9b7479928e
mchamoudadev <mchamuuda@gmail.com>
2023-10-06 01:17:25 +0300

    added base url for delete and update buttons

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'https://nextjs-getting-started-1gz8ac5zh-jtechdev.vercel.app/:path*',
+            },
+        ];
+    },
     images: {
         remotePatterns: [
             {
